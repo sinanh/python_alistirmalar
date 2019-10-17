72fdff037ee101dd0f4ba864d16b31f7383ac9d8
Sinan Hanay <sinanh@users.noreply.github.com>
2019-10-17 12:12:26 +0300

    Update boolean.py

--- AkışKontrolü/boolean.py
+++ AkışKontrolü/boolean.py
@@ -6,15 +6,9 @@ Created on Sun Sep 29 13:29:02 2019
 @author: sinan
 """
 
-
- 
 SIFRE = 'şafak'
 
 girilen_sifre = input('şifrenizi girin')
 esit_mi = girilen_sifre == SIFRE
 
-print('girilen gerçek şifreye eşit mi: ', esit_mi )
-
-
-
- 
+print('girilen gerçek şifreye eşit mi: ', esit_mi)
