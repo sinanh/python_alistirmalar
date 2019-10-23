414754fb6db9eda6198dc752866c56ae88b65f03
Muhammed Batuhan Aydın <56546166+batuAyd@users.noreply.github.com>
2019-10-23 04:04:03 +0300

    Problem 2

--- AkışKontrolü/Triangle creation rule code.py
+++ AkışKontrolü/Triangle creation rule code.py
@@ -9,7 +9,7 @@ print('-'*50)
 
 if side_1 + side_2 >= side_3 or side_1 + side_3 >= side_2 or side_2 + side_3 >= side_1:
     print('Complies with triangle creation rule')
-
+ 
 elif side_1 == side_2 == side_3:
     print('Complies with triangle creation rule')
 
