f595115749c9c9585906a79af2e3dae8a78b6fb6
Muhammed Batuhan Aydın <56546166+batuAyd@users.noreply.github.com>
2019-10-27 02:28:47 +0300

    Update Triangle creation rule code.py

--- AkışKontrolü/Triangle creation rule code.py
+++ AkışKontrolü/Triangle creation rule code.py
@@ -1,13 +1,14 @@
+import math
 print('Triangle creation rule code')
 print('-'*50)
-side_1 = int(input('Enter the first side value '))
+side_1 = abs(int(input('Enter the first side value ')))
 print('-'*50)
-side_2 = int(input('Enter the second side value '))
+side_2 = abs(int(input('Enter the second side value ')))
 print('-'*50)
-side_3 = int(input('Enter the third side value '))
+side_3 = abs(int(input('Enter the third side value ')))
 print('-'*50)
 
-if side_1 + side_2 < side_3 or side_1 + side_3 < side_2 or side_2 + side_3 < side_1:
+if side_1 - side_2 < side_3 < side_1 + side_2 and side_1 - side_3 < side_2 < side_1 + side_3 and side_2 - side_3 < side_1 < side_2 + side_3 :
     print('Complies with triangle creation rule')
 
 elif side_1 == side_2 == side_3:
