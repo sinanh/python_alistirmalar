d2a3ebc3a47d2f351740cc79ca8be4117c285f17
Ömer Faruk Topaloğlu <34956568+oftopaloglu@users.noreply.github.com>
2019-10-27 01:36:27 +0300

    Akış kontrolü örneği.

---
+++ AkışKontrolü/Üçgen mi dörtgen mi.py
@@ -0,0 +1,37 @@
+import math
+
+
+şekil = input("Üçgen mi Dörtgen mi?")
+
+if (şekil == "dörtgen") :
+    print("Dört tane kenar girin.")
+    a = float(input("İlk kenarı gir."))
+    b = float(input("İkinci kenarı gir."))
+    c = float(input("Üçüncü kenarı gir."))
+    d = float(input("Dördüncü kenarı gir."))
+
+    if a == b and a == c and a == d :
+        print("Kare")
+    elif a == b and c ==d or a == c and b == d or a == d and b == c:
+        print("Dikdörtgen")
+    else:
+        print("Dörtgen")
+
+elif (şekil == "üçgen"):
+    print("Üç tane kenar girin.")
+    a = float(input("İlk kenarı gir."))
+    b = float(input("İkinci kenarı gir."))
+    c = float(input("Üçüncü kenarı gir."))
+
+    if abs(a-b)< c < abs(a+b) and abs(a-c) < b < abs(a+c) and abs(b-c) < a < abs(a+c):
+        if a == b and a == c:
+            print("Eşkenar üçgen.")
+        elif a == b and a != c or a == c and a != b:
+            print("İkizkenar üçgen")
+        else:
+            print("Çeşitkenar üçgen.")
+    else:
+        print("Üçgen belirtmiyor.")
+
+else:
+    print("Şekil tanımlanamadı.")
