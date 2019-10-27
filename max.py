b8f9caf0a0e7c5e55bc157a8ea6a1996bea83647
Ömer Faruk Topaloğlu <34956568+oftopaloglu@users.noreply.github.com>
2019-10-27 22:51:17 +0300

    Çözümler

---
+++ max.py
@@ -0,0 +1,7 @@
+sayi_1 = float(input("İlk sayıyı giriniz."))
+sayi_2 = float(input("İkinci sayıyı giriniz."))
+sayi_3 = float(input("Üçüncü sayıyı giriniz."))
+
+liste = [sayi_1, sayi_2, sayi_3]
+
+print(max(liste))
