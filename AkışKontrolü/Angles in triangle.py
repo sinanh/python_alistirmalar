be97b82931b49a985b5855a4f21a65f8a72e4ad7
Muhammed Batuhan Aydın <56546166+batuAyd@users.noreply.github.com>
2019-10-27 22:43:43 +0300

    Problem 3

--- AkışKontrolü/Angles in triangle.py
+++ AkışKontrolü/Angles in triangle.py
@@ -1,17 +1,19 @@
+import math
+
 print('Angles in Triangle Code')
 print('-'*50)
 
-angle_1 = int(input('Enter the first angle value: '))
+angle_1 = abs(float(input('Enter the first angle value: ')))
 print('-'*50)
 
-angle_2 = int(input('Enter the second angle value: '))
+angle_2 = abs(float(input('Enter the second angle value: ')))
 print('-'*50)
 
-angle_3 = int(input('Enter the third angle value: '))
+angle_3 = abs(float(input('Enter the third angle value: ')))
 print('-'*50)
 
-if angle_1 + angle_2 + angle_3 == 180:
-    print('triangle can be drawn ')
+if (angle_1 and angle_2 and angle_3 != 0) and angle_1 + angle_2 + angle_3 == 180  :
+    print('triangle can be drawn')
 
 else:
-    print('triangle can not be drawn ')
+    print('triangle can not be drawn')
