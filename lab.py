5a132bbc461b60ab42be33e5e56c5caa23e39062
Kursadcc <72594820+Kursadcc@users.noreply.github.com>
2022-03-07 11:28:03 +0300

    Delete lab.py

--- lab.py
+++
@@ -1,7 +0,0 @@
-def isUpperCase(x):
-    if x.isupper():
-        print("kelimede büyük harfler vardır")
-    if x.islower():
-        print("girilen kelime küçük harflerden oluşur")
-
-isUpperCase("ANKARA")
